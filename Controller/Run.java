671b7a430da33f37c4f827af9ad31ff79b574137
Kami Hassanzadeh <kamihassanzadeh@Kamis-MacBook-Pro.local>
2017-10-24 14:35:14 +0200

    update

--- Controller/Run.java
+++
@@ -1,8 +0,0 @@
-package com.jensen.Controller;
-
-public class Run {
-
-	public static void main(String[] args) {	
-		new Application();
-	}
-}
--- View/panels/Panel.java
+++
@@ -1,8 +0,0 @@
-package com.jensen.View.panels;
-
-import javax.swing.JPanel;
-
-public interface Panel {
-	public JPanel getPanel();
-	public void update();
-}
